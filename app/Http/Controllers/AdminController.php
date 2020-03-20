88862b20601b9d60e023123ce1a37dca0721554d
GabrielFemi <gabrielfemi799@gmail.com>
2020-03-20 15:12:29 +0100

    added comments

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -49,6 +49,9 @@ class AdminController extends Controller
         $request->validate([
             'phone_number' => 'bail|required|numeric|digits:11'
         ]);
+
+        //Store the Phone number of the student in the database
+
         $store_student = new Student;
         $phone_number = $request->phone_number;
         $store_student->phone_number = $phone_number;
@@ -101,6 +104,12 @@ class AdminController extends Controller
         //
     }
 
+    /**
+     * @param
+     * @param \Illuminate\Http\Request  $request
+     * Save message to the database and send it to students.
+     */
+    
     public function send(Request $request)
     {
         
@@ -114,58 +123,46 @@ class AdminController extends Controller
         
         //$new_message->save();
         // if ($new_message->save();) {
-            # do the next tablecode...
-            // if that next table is successful then redirect else whatever      }
-        
+        # do the next tablecode...
+        // if that next table is successful then redirect else whatever      }
 
-        //Messaging REST API
-        $message = Message::where('message',$request->get('message'))->select('message')->first();
+        $message = Message::where('message',$request->get('message'))->select('message')->get();
         //return $message;
         $phone_numbers = Student::all('phone_number');
-        
-        
-        
-        
-
-        
 
 
-try {
-// Your Account SID and Auth Token from twilio.com/console
-    $account_sid = 'AC45a2dad812bb662dc11a77335d38c14a';
-    $auth_token = '535569c8159f51d75d36ab91c92ffaf9';
-    // In production, these should be environment variables. E.g.:
-    // $auth_token = $_ENV["TWILIO_AUTH_TOKEN"]
+//Messaging REST API
+    try {
+    // Your Account SID and Auth Token from twilio.com/console
+        $account_sid = 'AC45a2dad812bb662dc11a77335d38c14a';
+        $auth_token = '535569c8159f51d75d36ab91c92ffaf9';
+        // In production, these should be environment variables. E.g.:
+        // $auth_token = $_ENV["TWILIO_AUTH_TOKEN"]
 
-    // A Twilio number you own with SMS capabilities
-    $twilio_number = "+12057934134";
+        // A Twilio number you own with SMS capabilities
+        $twilio_number = "+12057934134";
 
-    $client = new Client($account_sid, $auth_token);
-    if($client->messages->create(
-        // Where to send a text message (your cell phone?)
-        '+234812695004224',
-        array(
-            'from' => $twilio_number,
-            'body' => 'Testing my Twilio API'
-        )
-    )){
+        $client = new Client($account_sid, $auth_token);
+        if($client->messages->create(
+            // Where to send a text message (your cell phone?)
+        $phone_numbers[0],
+            array(
+                'from' => $twilio_number,
+                'body' => 'Testing my Twilio API'
+            )
+        )){
+            
+            return back()->with('success','Message successfully sent');
+        }
+            
+            
         
-        return back()->with('success','Message successfully sent');
+        
+    }catch (\Exception $e) {
+        
+        // will return user to previous screen with twilio error
+        return back()->withError($e->getMessage());
     }
-        
-        
-    
-    
-}catch (\Exception $e) {
-    
-    // will return user to previous screen with twilio error
-    return back()->withError($e->getMessage());
-}
 
-        
-        
-
-
-
-    }
+        }
 }
