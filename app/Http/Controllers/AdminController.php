989cd85f2b65ead775fc5b312375b8e94d94ed18
GabrielFemi <gabrielfemi799@gmail.com>
2020-03-20 11:48:52 +0100

    api is successfully working

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -121,17 +121,39 @@ class AdminController extends Controller
         $message = Message::where('message',$request->get('message'))->select('message')->first();
         //return $message;
         $phone_numbers = Student::all('phone_number');
-        return $phone_numbers;
+        
         
         
         
 
         $username = 'gabrielfemi';
         $password = 'Portedjester_20';
-        $decoded_message = json_decode($message);
         $messages = array(
-        array('to'=>['08126950044'], 'body'=>'Hello World!')
+        array('to'=>'+2348167345364', 'body'=>'Hello World!')
         );  
+        function send_message ( $post_body, $url, $username, $password) {
+            $ch = curl_init( );
+            $headers = array(
+            'Content-Type:application/json',
+            'Authorization:Basic '. base64_encode("$username:$password")
+            );
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+            curl_setopt ( $ch, CURLOPT_URL, $url );
+            curl_setopt ( $ch, CURLOPT_POST, 1 );
+            curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, 1 );
+            curl_setopt ( $ch, CURLOPT_POSTFIELDS, $post_body );
+            // Allow cUrl functions 20 seconds to execute
+            curl_setopt ( $ch, CURLOPT_TIMEOUT, 20 );
+            // Wait 10 seconds while trying to connect
+            curl_setopt ( $ch, CURLOPT_CONNECTTIMEOUT, 10 );
+            $output = array();
+            $output['server_response'] = curl_exec( $ch );
+            $curl_info = curl_getinfo( $ch );
+            $output['http_status'] = $curl_info[ 'http_code' ];
+            $output['error'] = curl_error($ch);
+            curl_close( $ch );
+            return $output;
+            } 
 
         $result = send_message( json_encode($messages), 'https://api.bulksms.com/v1/messages?auto-unicode=true&longMessageMaxParts=30', $username, $password );
 
@@ -142,31 +164,8 @@ class AdminController extends Controller
         // Use json_decode($result['server_response']) to work with the response further
         }
 
-        function send_message ( $post_body, $url, $username, $password) {
-        $ch = curl_init( );
-        $headers = array(
-        'Content-Type:application/json',
-        'Authorization:Basic '. base64_encode("$username:$password")
-        );
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        curl_setopt ( $ch, CURLOPT_URL, $url );
-        curl_setopt ( $ch, CURLOPT_POST, 1 );
-        curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, 1 );
-        curl_setopt ( $ch, CURLOPT_POSTFIELDS, $post_body );
-        // Allow cUrl functions 20 seconds to execute
-        curl_setopt ( $ch, CURLOPT_TIMEOUT, 20 );
-        // Wait 10 seconds while trying to connect
-        curl_setopt ( $ch, CURLOPT_CONNECTTIMEOUT, 10 );
-        $output = array();
-        $output['server_response'] = curl_exec( $ch );
-        $curl_info = curl_getinfo( $ch );
-        $output['http_status'] = $curl_info[ 'http_code' ];
-        $output['error'] = curl_error($ch);
-        curl_close( $ch );
-        return $output;
-        } 
         
-        return redirect()->back()->with('success', 'Your message has been sent!');
+        
 
 
 
