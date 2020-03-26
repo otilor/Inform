3329f83ce4af31eba4c1e1640fe75f68c45400b2
GabrielFemi <gabrielfemi799@gmail.com>
2020-03-26 17:45:46 +0100

    Created new migration for all admins

--- database/migrations/2020_03_26_163757_create_admins.php
+++ database/migrations/2020_03_26_163757_create_admins.php
@@ -17,7 +17,7 @@ class CreateAdmins extends Migration
             $table->id();
             $table->bigInteger('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->nullable();
-            $table->integer('role_id');
+            $table->integer('role_id')->default(0);
             $table->timestamps();
         });
     }
